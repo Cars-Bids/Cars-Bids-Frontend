5f53f470051b7aff9e001befb29ec1cc8a3dddd7
Reap4ick <kiryukha.romanyuk@gmail.com>
2025-06-03 20:34:56 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h1 className="text-6xl font-extrabold bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 text-transparent bg-clip-text animate-pulse drop-shadow-lg tracking-wide text-center mt-10">
+      TailwindCSS
+    </h1>
+    </>
+  )
+}
+
+export default App
