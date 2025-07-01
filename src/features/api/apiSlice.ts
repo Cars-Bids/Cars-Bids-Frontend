a25a42c201e87c4d6953697f0308c00d20b613f3
Aernjdz <zarichniukandriy5@gmail.com>
2025-07-01 17:22:43 +0300

    feat(api)  integrate base URL from api_conf.json

--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -1,9 +1,9 @@
 
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-
+import apiConf from "../../conf/api_conf.json";
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://' }),
+  baseQuery: fetchBaseQuery({ baseUrl: apiConf.baseUrl }),
    endpoints: () => ({})
 });
 
