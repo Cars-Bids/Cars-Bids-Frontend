9f731f1b9812c56c4df46c3e4caf2a3874e96208
Voinore1 <vovavoino3@gmail.com>
2025-09-15 19:54:53 +0300

    fixed sell car page. Now working.

--- src/features/api/endpoints/CarEndpoints.ts
+++ src/features/api/endpoints/CarEndpoints.ts
@@ -5,14 +5,19 @@ import {serialize} from "object-to-formdata";
 export const CarEndpoints = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         requestNewCar: builder.mutation<void, RequestNewCarCommand>({
-            query: (data) => {
-
+            query: ({photos, ...data}) => {
                 console.log(data)
                 data.userId = 1;
+
+                const formData = serialize(data);
+                photos.forEach((photo) => {
+                    formData.append('photos', photo);
+                })
+
                 return {
                     url: "/Car/requestNewCar",
                     method: "POST",
-                    body: serialize(data),
+                    body: formData,
                 };
             },
         }),
